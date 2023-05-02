7321fb1191c3851a71a58ac2d4c72572117fed8d
Tagellajore <tagelsft9@gmail.com>
2023-05-01 21:49:11 -0400

    we check console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "  Assertion Passed: " + actual + " === " + expected;
+  } else if (actual !== expected) {
+    return "Assertion Failed: " + actual + " !== " + expected;
+  }
+};
+
+//Test code
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(2, 1));
+console.log(assertEqual("tagel", "tagel"));
