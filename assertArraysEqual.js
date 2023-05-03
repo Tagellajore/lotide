bd12431ed2aa4603b19a7f7d26fee685d3e0193f
Tagellajore <tagelsft9@gmail.com>
2023-05-03 15:06:30 -0400

    implemented middle array function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -11,11 +11,20 @@ const eqArrays = function(arr1, arr2) {
   }
 
   const assertArraysEqual = function(actual, expected) {
-    if (actual === expected) {
+    if(actual.length !== expected.length) {
+      return `Assertion Failed : ${actual} !== ${expected}`;
+    }
+    for (let i = 0; i < actual.length; i++) {
+      if(actual[i] !== expected[i]) {
+      return `Assertion Failed : ${actual} !== ${expected}`;  
+      }
+    }
+    return `Assertion Passed : ${actual} === ${expected}`;
+    /*if (actual === expected) {
       return `Assertion Passed : ${actual} === ${expected}`;
     } else {
       return `Assertion Failed : ${actual} !== ${expected}`;
-    }
+    }*/
   }
 
   console.log(assertArraysEqual(eqArrays([1,2,3], [1,2,2]), true));
---
+++ middle.js
@@ -0,0 +1,53 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } 
+  for (let i = 0; i <arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+  }
+
+
+const assertArraysEqual = function(actual, expected) {
+  if(actual.length !== expected.length) {
+    return `Assertion Failed : ${actual} !== ${expected}`;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if(actual[i] !== expected[i]) {
+    return `Assertion Failed : ${actual} !== ${expected}`;  
+    }
+  }
+  return `Assertion Passed : ${actual} === ${expected}`;
+  /*if (actual === expected) {
+    return `Assertion Passed : ${actual} === ${expected}`;
+  } else {
+    return `Assertion Failed : ${actual} !== ${expected}`;
+  }*/
+}
+
+
+const middle = function(array) {
+  let midIndex = Math.floor(array.length/2);
+  if (array.length <= 2) {
+    return [];
+  }
+  if (array.length % 2 === 0) {
+    return [array[midIndex - 1], array[midIndex]];
+  } else {
+    return [array[midIndex]];
+  }
+}
+
+/*console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));*/
+
+
+console.log(assertArraysEqual(middle([1,2,3]) , [2]));
+console.log(assertArraysEqual(middle([1,2,3]) , [3]));
