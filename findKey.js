92e0b9037e53a34c907921396d94c9edd60d3a92
Tagellajore <tagelsft9@gmail.com>
2023-05-05 08:39:58 -0400

    findKey

---
+++ findKey.js
@@ -0,0 +1,56 @@
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+console.log(result);
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)
+
+console.log(result2);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `Assertion Passed: ${actual}  ===  ${expected}`;
+  } else {
+    return `Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+
+console.log(assertEqual(result, "noma"));
+
+console.log(assertEqual(result2, "noma"));
