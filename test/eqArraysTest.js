b8806109f104b645139691eeddcc1b2fbc08f34e
Tagellajore <tagelsft9@gmail.com>
2023-05-08 13:29:16 -0400

    refactored eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+
+const eqArrays = require('../eqArrays')
+
+
+
+
+
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true));
