0d479755d660abbac8ca78dec6b4d1fea5ae27a8
Tagellajore <tagelsft9@gmail.com>
2023-05-08 14:04:36 -0400

    refactored assertArraysEqual.js with modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,19 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (let i = 0; i <arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-  }
+
+const eqArrays = require('./eqArrays')
+
 
   const assertArraysEqual = function(actual, expected) {
-    if(actual.length !== expected.length) {
-      return `Assertion Failed : ${actual} !== ${expected}`;
-    }
-    for (let i = 0; i < actual.length; i++) {
-      if(actual[i] !== expected[i]) {
-      return `Assertion Failed : ${actual} !== ${expected}`;  
-      }
-    }
-    return `Assertion Passed : ${actual} === ${expected}`;
-    /*if (actual === expected) {
-      return `Assertion Passed : ${actual} === ${expected}`;
+    if (actual === expected) {
+      return `Assertion Passed: ${actual} === ${expected}`;
     } else {
-      return `Assertion Failed : ${actual} !== ${expected}`;
-    }*/
+      return `Assertion Failed: ${actual} !== ${expected}`;
+    }
   };
 
   console.log(assertArraysEqual(eqArrays([1,2,3], [1,2,2]), true));
   console.log(assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true));
+
+
+  module.exports = assertArraysEqual;
 
   
