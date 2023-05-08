d77256a3662b6e68ae45457da1cce533c8977703
Tagellajore <tagelsft9@gmail.com>
2023-05-08 14:50:21 -0400

    made some improvements in assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,15 +3,16 @@ const eqArrays = require('./eqArrays')
 
 
   const assertArraysEqual = function(actual, expected) {
-    if (actual === expected) {
+    console.log(actual, expected)
+    if (eqArrays(actual, expected)) {
+
       return `Assertion Passed: ${actual} === ${expected}`;
     } else {
       return `Assertion Failed: ${actual} !== ${expected}`;
     }
   };
 
-  console.log(assertArraysEqual(eqArrays([1,2,3], [1,2,2]), true));
-  console.log(assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true));
+
 
 
   module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -49,5 +49,6 @@ console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5, 6]));*/
 
 
-console.log(assertArraysEqual(middle([1,2,3]) , [2]));
-console.log(assertArraysEqual(middle([1,2,3]) , [3]));
+
+
+module.exports = middle;
