1d77ca5517480de305396a5ca1ee07fa0eb15f99
Tagellajore <tagelsft9@gmail.com>
2023-05-08 14:48:54 -0400

    refactored middle using modules

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+const middle = require('../middle');
+
+
+console.log(assertArraysEqual(middle([1,2,3]) , [2]));
+console.log(assertArraysEqual(middle([1,2,3]) , [10]));
